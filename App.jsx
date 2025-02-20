fc2d5f7ead19eb2def7662e714597f8da0dd1f27
Hrishikeshmind <149176033+Hrishikeshmind@users.noreply.github.com>
2025-02-20 10:54:43 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
